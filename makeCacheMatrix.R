cccda869fac3149670e9c8febe8887f37d257b35
Emmi Saarikoski <44564729+emmisaarikoski@users.noreply.github.com>
2020-06-29 18:56:40 +0300

    Programming assignment 2: Lexical scoping
    I added this here, because I got email that said my submission is incomplete. I did my submission earlier in the "cachematrix.R"-file.

---
+++ makeCacheMatrix.R
@@ -0,0 +1,33 @@
+makeCacheMatrix <- function(x = matrix()) { 
+# Function takes matrix type argument, example: 
+# makeCacheMatrix(matrix(1:20,nrow=5,ncol=4))
+    Value_of_inverse <- NULL
+    set_value_of_matrix <- function(y) {
+        x <<- y
+        Value_of_inverse <<- NULL # Read the help file: ?"<<-"
+    }
+    get_value_of_matrix <- function() #get the given matrix: variable x
+        {x}
+    Inverse_Set <- function(inverse) 
+        {Value_of_inverse <- inverse}
+    Inverse_Get <- function() 
+    {Value_of_inverse}
+    list(set_value_of_matrix = set_value_of_matrix, 
+         get_value_of_matrix = get_value_of_matrix, 
+         Inverse_Set=Inverse_Set, Inverse_Get=Inverse_Get )
+}
+
+#cacheSolve calculates the matrix inverse
+cacheSolve <- function(x, ...) {
+    Value_of_inverse <- x$Inverse_Get()
+    if(!is.null(Value_of_inverse)) { 
+#if value is not null: give message, and return calculated inverse
+        message("getting cached data")
+        return(Value_of_inverse)
+    }
+    data <- x$get_value_of_matrix()
+    Value_of_inverse <- solve(data, ...) 
+    #the standard R function for matrix inverse is solve()
+    x$Inverse_Set(Value_of_inverse) #set calculated inverse to value_of_inverse
+    Value_of_inverse
+}
